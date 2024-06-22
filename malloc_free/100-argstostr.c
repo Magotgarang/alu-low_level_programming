a34297dd52c6485103487e56bb20bbaac05b31f7
Magotgarang <m.guut@alustudent.com>
2024-06-22 12:39:54 -0700

    updated

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -1,36 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * argstostr - Concatenates all the arguments of your program.
+ * @ac: The number of arguments.
+ * @av: The array of arguments.
+ *
+ * Return: A pointer to a new string, or NULL if it fails.
+ */
 char *argstostr(int ac, char **av)
 {
-    int i, j, k = 0, len = 0;
-    char *s;
+	int i, j, k = 0, len = 0;
+	char *s;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    /* Calculate the total length required for the concatenated string */
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-            len++;
-        len++; /* for the newline character */
-    }
+	/* Calculate the total length required for the concatenated string */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++; /* for the newline character */
+	}
 
-    s = malloc(sizeof(char) * (len + 1));
-    if (s == NULL)
-        return (NULL);
+	s = malloc(sizeof(char) * (len + 1));
+	if (s == NULL)
+		return (NULL);
 
-    /* Copy the arguments into the new string */
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-        {
-            s[k++] = av[i][j];
-        }
-        s[k++] = '\n'; /* Add a newline character after each argument */
-    }
-    s[k] = '\0'; /* Null-terminate the string */
+	/* Copy the arguments into the new string */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			s[k++] = av[i][j];
+		}
+		s[k++] = '\n'; /* Add a newline character after each argument */
+	}
+	s[k] = '\0'; /* Null-terminate the string */
 
-    return (s);
+	return (s);
 }
