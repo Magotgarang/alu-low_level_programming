c238f59e11ae91d5fe43ca34a2a5a104e91d2b9a
Magotgarang <m.guut@alustudent.com>
2024-06-22 09:43:02 -0700

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,29 +3,38 @@
 
 /**
  * _strdup - Duplicate a string
- * @str: the stringto duplicate
+ * @str: the string to duplicate
  *
- * Return: the string duplicated
+ * Return: the duplicated string
  */
-
 char *_strdup(char *str)
 {
-	int a = 0, i = 0;
-	char *s;
-	if (str == NULL)
-		return (NULL);
-	while (str[i])
-	{
-		i++;
-	}
-	s = malloc((sizeof(char) * i) + 1);
-	if (s == NULL)
-		return (NULL);
-	while (a < i)
-	{
-		s[a] = str[a];
-		a++;
-	}
-	s[a] = '\0';
-	return (s);
+    int a = 0, i = 0;
+    char *s;
+
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+
+    while (str[i])
+    {
+        i++;
+    }
+
+    s = malloc((sizeof(char) * i) + 1);
+
+    if (s == NULL)
+    {
+        return (NULL);
+    }
+
+    while (a < i)
+    {
+        s[a] = str[a];
+        a++;
+    }
+
+    s[a] = '\0';
+    return (s);
 }
