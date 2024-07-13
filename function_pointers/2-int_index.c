8898a77c3c1325a41da345d1a596fe6c9124e56e
Magotgarang <m.guut@alustudent.com>
2024-07-13 13:18:24 -0700

    updated

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -3,7 +3,7 @@
 /**
  * int_index - return index place if comparison = true, else -1
  * @array: array
- * @size: size of element in array 
+ * @size: size of element in array
  * @cmp: pointers to function of one of the 3 in main
  * Return: 0
  */
@@ -11,10 +11,10 @@
 int int_index(int *array, int size, int(*cmp)(int))
 {
 	int i;
-	
+
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
