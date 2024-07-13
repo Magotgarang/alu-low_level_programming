65164652cca443950bc084dabc587e096468b20e
Magotgarang <m.guut@alustudent.com>
2024-07-13 12:34:31 -0700

    updated

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
- * array_iterator - prints each array element on a new line
- * @array: the array to be printed
- * @size: the number of elements in the array
- * @action: pointer to the function used to print the elements
- *
- * Description: This function takes an array, its size, and a function
- * pointer as arguments. It iterates through the array and applies the
- * function pointed to by action to each element.
- */
+* array_iterator - prints each array element on a new line
+* @array: the array to be printed
+* @size: the number of elements in the array
+* @action: pointer to the function used to print the elements
+*
+* Description: This function takes an array, its size, and a function
+* pointer as arguments. It iterates through the array and applies the
+* function pointed to by action to each element.
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
     unsigned int i;
